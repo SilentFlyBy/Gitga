5141316d0655b969c3c95adf786a0bf166c2d7a5
Erik Moldtmann <erik.moldtmann@it-tecture.de>
2018-03-07 09:58:49 +0100

    optimize git command string building

--- src/core/git/command/git-command-builder.ts
+++ src/core/git/command/git-command-builder.ts
@@ -4,10 +4,19 @@ import {GitCommand, ICommitCommand, IGitCommand} from "./";
 export class GitCommandBuilder {
     public static BuildCommand<T extends IGitCommand>(command: GitCommand<T>): string {
         const commandString = command.Command;
+        const argString = command.Argument;
         let parameterString = "";
 
         for (const param of command.CommandParameters) {
-            parameterString += " " + param.ToParameterString();
+            if (parameterString.length === 0) {
+                parameterString = param.ToParameterString();
+            } else {
+                parameterString = `${parameterString} ${param.ToParameterString()}`;
+            }
+        }
+
+        if (argString && argString.length > 0) {
+            return `${commandString} ${parameterString} ${argString}`;
         }
 
         return `${commandString} ${parameterString}`;
