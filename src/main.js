ca08f9de5a407674f912719c5618b59da544605a
Erik Moldtmann <erik@moldtmann.de>
2018-03-13 00:31:39 +0100

    add vscode debugging

--- src/main.js
+++ src/main.js
@@ -33,6 +33,8 @@ function createWindow () {
   })
 }
 
+app.commandLine.appendSwitch('remote-debugging-port', '9222')
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
