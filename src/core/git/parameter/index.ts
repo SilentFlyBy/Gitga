d3c72c624c2c6cdac4bd373597df9beec3a755a2
Erik Moldtmann <erik@moldtmann.de>
2018-03-14 21:55:24 +0100

    fix broken build

--- src/core/git/parameter/index.ts
+++ src/core/git/parameter/index.ts
@@ -2,7 +2,6 @@ import {IGitCommand} from "../command";
 
 export * from "./commit-parameter";
 export * from "./status-parameter";
-export * from "./short-parameter";
 export * from "./long-parameter";
 export * from "./key-value-parameter";
 
