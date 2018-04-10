c3e7aaa81596e5b72a2a085f1143b4f5db6d4d26
Erik Moldtmann <erik@moldtmann.de>
2018-04-10 15:44:02 +0200

    import electron conditionally

--- src/browser/events/index.ts
+++ src/browser/events/index.ts
@@ -1,9 +1,16 @@
-import * as electron from "electron";
 import { Store } from "react-redux";
 import { OpenRepository } from "../actions/repository";
+import * as electron from "electron";
 
 export default function configureEvents<S>(store: Store<S>) {
-    const listener = electron.ipcRenderer;
+    if (process.versions.electron) {
+        return configureElectronEvents(store);
+    }
+}
+
+function configureElectronEvents<S>(store: Store<S>) {
+    const el = require("electron");
+    const listener = el.ipcRenderer;
 
     listener.on("openRepository", (event: any, message: string) => {
         store.dispatch(OpenRepository(message));
